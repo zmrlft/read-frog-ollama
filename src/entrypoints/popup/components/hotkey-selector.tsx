83894f8120a243244bd80a79af1b52bebb1613c1
Kuiliang Zhang <z1219202167@outlook.com>
2025-05-11 12:24:47 -0700

    feat: add i18n for hover in popup

--- src/entrypoints/popup/components/hotkey-selector.tsx
+++ src/entrypoints/popup/components/hotkey-selector.tsx
@@ -26,15 +26,17 @@ export default function HotkeySelector() {
           className="text-[13px] truncate pl-0 font-medium shadow-none cursor-pointer border-none bg-transparent ring-none focus-visible:border-none focus-visible:ring-0 hover:bg-transparent dark:hover:bg-transparent dark:bg-transparent"
         >
           <div className="truncate">
-            Hover + {HOTKEY_ITEMS[manualTranslate.hotkey].icon}{" "}
-            {HOTKEY_ITEMS[manualTranslate.hotkey].label} to translate paragraph
+            {i18n.t("popup.hover")} +{" "}
+            {HOTKEY_ITEMS[manualTranslate.hotkey].icon}{" "}
+            {HOTKEY_ITEMS[manualTranslate.hotkey].label}{" "}
+            {i18n.t("popup.translateParagraph")}
           </div>
         </SelectTrigger>
         <SelectContent>
           {HOTKEYS.map((item) => (
             <SelectItem key={item} value={item}>
-              + {HOTKEY_ITEMS[item].icon} {HOTKEY_ITEMS[item].label} to
-              translate paragraph
+              {i18n.t("popup.hover")} + {HOTKEY_ITEMS[item].icon}{" "}
+              {HOTKEY_ITEMS[item].label} {i18n.t("popup.translateParagraph")}
             </SelectItem>
           ))}
         </SelectContent>
