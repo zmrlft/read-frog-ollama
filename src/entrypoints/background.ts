629aee340f3043e18018f91ce40a950b59fc06cc
Kuiliang Zhang <z1219202167@outlook.com>
2025-05-12 00:05:02 -0700

    chore: pop user guide

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,9 +1,16 @@
 export default defineBackground(() => {
   logger.info("Hello background!", { id: browser.runtime.id });
 
-  browser.runtime.onInstalled.addListener(async () => {
+  browser.runtime.onInstalled.addListener(async (details) => {
     await storage.setItem<number>("local:__configSchemaVersion", 1);
     await initializeConfig();
+
+    // Open tutorial page when extension is installed
+    if (details.reason === "install") {
+      await browser.tabs.create({
+        url: "https://readfrog.mengxi.work/tutorial/installation",
+      });
+    }
   });
 
   browser.runtime.onMessage.addListener((message) => {
