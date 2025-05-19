63b85034d6bedd5074695b119afe116345366028
Kuiliang Zhang <z1219202167@outlook.com>
2025-05-19 12:36:19 -0700

    fix: hidden button text in dark mode

--- src/entrypoints/side.content/components/floating-button/components/hidden-button.tsx
+++ src/entrypoints/side.content/components/floating-button/components/hidden-button.tsx
@@ -12,7 +12,7 @@ export default function HiddenButton({
   return (
     <button
       type="button"
-      className="border-border mr-2 translate-x-12 cursor-pointer rounded-full border bg-white p-1.5 text-neutral-600 transition-transform duration-300 group-hover:translate-x-0 hover:bg-neutral-100 dark:bg-neutral-900 dark:hover:bg-neutral-800"
+      className="border-border mr-2 translate-x-12 cursor-pointer rounded-full border bg-white p-1.5 text-neutral-600 dark:text-neutral-400 transition-transform duration-300 group-hover:translate-x-0 hover:bg-neutral-100 dark:bg-neutral-900 dark:hover:bg-neutral-800"
       onClick={onClick}
     >
       <Icon className="h-5 w-5" strokeWidth={1.8} />
