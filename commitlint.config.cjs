dfede5757a6224152fe994d3ee8e53d81e73cdcc
Kuiliang Zhang <z1219202167@outlook.com>
2025-05-19 21:01:31 -0700

    ci: improve commit lint and add pr template

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,26 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        // Default types from conventional commits
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        // Custom types from PR template
+        'i18n',
+        'ai',
+      ],
+    ],
+  },
+}
