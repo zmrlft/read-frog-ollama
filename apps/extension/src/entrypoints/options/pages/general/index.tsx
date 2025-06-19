dbf42cd4ceb0632f6e857c06c04444518f10abf9
taiiiyang <56546332+taiiiyang@users.noreply.github.com>
2025-06-19 10:55:32 +0800

    feat: add reset config button (#165)
    * feat: add reset config bottom

--- apps/extension/src/entrypoints/options/pages/general/index.tsx
+++ apps/extension/src/entrypoints/options/pages/general/index.tsx
@@ -1,5 +1,6 @@
 import { PageLayout } from '../../components/page-layout'
 import { ReadConfig } from './read-config'
+import { ResetConfig } from './reset-config'
 import TranslationConfig from './translation-config'
 
 export function GeneralPage() {
@@ -7,6 +8,7 @@ export function GeneralPage() {
     <PageLayout title={i18n.t('options.general.title')} innerClassName="[&>*]:border-b [&>*:last-child]:border-b-0">
       <ReadConfig />
       <TranslationConfig />
+      <ResetConfig />
     </PageLayout>
   )
 }
