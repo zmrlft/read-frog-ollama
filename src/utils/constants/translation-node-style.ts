650cf04527d86b253c6f9afc6f0e965b7fe86731
WangYK <62269186+AnotiaWang@users.noreply.github.com>
2025-08-12 08:22:10 +0800

    feat: add translation style: `weakened` (#275)

--- src/utils/constants/translation-node-style.ts
+++ src/utils/constants/translation-node-style.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_TRANSLATION_NODE_STYLE = 'default'
 
-export const TRANSLATION_NODE_STYLE = [DEFAULT_TRANSLATION_NODE_STYLE, 'blur', 'blockquote'] as const
+export const TRANSLATION_NODE_STYLE = [DEFAULT_TRANSLATION_NODE_STYLE, 'blur', 'blockquote', 'weakened'] as const
 
 export const CUSTOM_TRANSLATION_NODE_ATTRIBUTE = 'read-frog-custom-translation-style'
