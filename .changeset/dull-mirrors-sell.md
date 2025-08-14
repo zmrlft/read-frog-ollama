92e855a8c2cddc4493d056df63d6f1ebe8d3ea58
ananaBMaster <68643891+ananaBMaster@users.noreply.github.com>
2025-08-14 10:43:27 -0700

    fix: don't print floating button on web page (#331)

---
+++ .changeset/dull-mirrors-sell.md
@@ -0,0 +1,5 @@
+---
+"@read-frog/extension": patch
+---
+
+fix: reduce concurrent request capacity
--- apps/extension/src/utils/constants/translate.ts
+++ apps/extension/src/utils/constants/translate.ts
@@ -2,4 +2,4 @@ export const MIN_TRANSLATE_RATE = 1
 export const MIN_TRANSLATE_CAPACITY = 1
 
 export const DEFAULT_REQUEST_RATE = 8
-export const DEFAULT_REQUEST_CAPACITY = 500
+export const DEFAULT_REQUEST_CAPACITY = 200
