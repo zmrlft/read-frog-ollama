a6f24e63cada3795d9059f378d10bed363325667
ananaBMaster <68643891+ananaBMaster@users.noreply.github.com>
2025-08-15 23:03:31 -0700

    docs(changeset): changeset record (#349)

--- src/entrypoints/host.content/app.tsx
+++ src/entrypoints/host.content/app.tsx
@@ -1,10 +1,9 @@
-import { cn } from '@repo/ui/lib/utils'
 import FrogToast from '@/components/frog-toast'
 import { NOTRANSLATE_CLASS } from '@/utils/constants/dom-labels'
 
 export default function App() {
   return (
-    <div className={cn('text-black dark:text-white', NOTRANSLATE_CLASS)}>
+    <div className={NOTRANSLATE_CLASS}>
       <FrogToast />
     </div>
   )
